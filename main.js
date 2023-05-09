b998add058ddd9a11a843a8fb362437c77a83983
ArthurSpada <113390584+ArthurSpada@users.noreply.github.com>
2023-05-09 19:36:21 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ menuListArray = ["Pizza Vegetariana",
 "Pizza de frango",
 "Pizza Portuguesa",
 "Pizza Quatro Queijos",
-"pizza de calabresa",
+"Pizza de calabresa",
 "Pizza Extravaganza"
                     ];
 
@@ -10,21 +10,21 @@ function getMenu(){
 var htmldata="";
 menuListArray.sort();
 for(var i=0;i<menuListArray.length;i++){
-    htmldata=htmldata + menuListArray[i]+ '<br>'
+    htmldata=htmldata + menuListArray[i]+ '<br>';
 }
  document.getElementById("displayMenu").innerHTML = htmldata;
 }
 
 function addItem(){
 var htmldata;
-var imgtags='<img id="im1" src="images/pizzaImg.png"/>'
+var imgtags='<img id="img" src="pizzaImg.png"/>'
 var item=document.getElementById("addItem").value;
 menuListArray.sort();
 htmldata=""
-for(var i=0;i<menuListArray.lenght;i++){
-    htmldata=htmldata + menuListArray[i]+ '<br>'
+for(var i=0;i<menuListArray.length;i++){
+    htmldata=htmldata+imgtags + menuListArray[i]+ '<br>';
 }
-
+  document.getElementById("displayAddedMenu").innerHTML = htmldata;
 }
 
 function addTop(){
